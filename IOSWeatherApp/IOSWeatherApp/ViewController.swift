706f3fb646d6830bef2188dae98d66cb22bf9827
Yeldos <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>
2026-04-10 14:00:04 +0500

    Initial project setup

---
+++ IOSWeatherApp/IOSWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSWeatherApp
+//
+//  Created by Eldos Kolbay on 10.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
