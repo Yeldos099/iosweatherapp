1a1fdda1d29112cdf3cc47384931f85ea1b15c2b
Yeldos <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>
2026-04-10 16:59:23 +0500

    add NetworkService and NetworkError

---
+++ IOSWeatherApp/Core/Network/NetworkError.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkError.swift
+//  IOSWeatherApp
+//
+//  Created by Eldos Kolbay on 10.04.2026.
+//
+
+import Foundation
+
+enum NetworkError: LocalizedError {
+    case noConnection
+    case timeout
+    case clientError(statusCode: Int)
+    case serverError(statusCode: Int)
+    case decodingError(Error)
+    case unknown(Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .noConnection:
+            return "Нет подключения к интернету"
+        case .timeout:
+            return "Превышено время ожидания"
+        case .clientError(let code):
+            return "Ошабка клиента: \(code)"
+        case .serverError(let code):
+            return "Ошибка сервера: \(code)"
+        case .decodingError(let error):
+            return "Ошибка декодирования: \(error.localizedDescription)"
+        case .unknown(let error):
+            return "Неизвестная ошибка: \(error.localizedDescription)"
+        
+        }
+    }
+}
---
+++ IOSWeatherApp/Core/Network/NetworkService.swift
@@ -0,0 +1,56 @@
+//
+//  NetworkService.swift
+//  IOSWeatherApp
+//
+//  Created by Eldos Kolbay on 10.04.2026.
+//
+
+import Foundation
+
+protocol NetworkServiceProtocol {
+    
+}
+
+final class NetworkService: NetworkServiceProtocol {
+    
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    
+    func request<T: Decodable>(url: URL) async throws -> T {
+        do {
+            let (data, response) = try await session.data(from: url)
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200...299: break
+                case 400...499:
+                    throw NetworkError.clientError(statusCode: httpResponse.statusCode)
+                case 500...599:
+                    throw NetworkError.serverError(statusCode: httpResponse.statusCode)
+                default: break
+                }
+            }
+            
+            do {
+                return try JSONDecoder().decode(T.self, from: data)
+            } catch {
+                throw NetworkError.decodingError(error)
+            }
+        } catch let error as NetworkError {
+            throw error
+        } catch let error as NSError {
+            switch error.code {
+            case NSURLErrorNotConnectedToInternet:
+                throw NetworkError.noConnection
+            case NSURLErrorTimedOut:
+                throw NetworkError.timeout
+            default:
+                throw NetworkError.unknown(error)
+            }
+        }
+    }
+}
