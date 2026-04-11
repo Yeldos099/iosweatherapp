3165405284f6e5e539896c1953b0c4bc0c6ed086
Yeldos <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>
2026-04-11 15:25:21 +0500

    add WeatherResponse model

---
+++ IOSWeatherApp/Features/Weather/Model/WeatherResponse.swift
@@ -0,0 +1,43 @@
+//
+//  WeatherResponse.swift
+//  IOSWeatherApp
+//
+//  Created by Eldos Kolbay on 11.04.2026.
+//
+
+import Foundation
+
+
+struct WeatherResponse: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    let wind: Wind
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let feelsLike: Double
+    let tempMin: Double
+    let tempMax: Double
+    let humidity: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case humidity
+    }
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Wind: Decodable {
+    let speed: Double
+}
