79ea1d3e99e59194d3e0f0d2618e9fff384ee2d5
Yeldos <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>
2026-04-18 18:05:59 +0500

    add WeatherViewModel adn WeatherMapper

---
+++ IOSWeatherApp/Features/Weather/Model/WeatherMapper.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherMapper.swift
+//  IOSWeatherApp
+//
+//  Created by Eldos Kolbay on 18.04.2026.
+//
+
+import Foundation
+
+
+final class WeatherMapper {
+    static func map(from response: WeatherResponse) -> WeatherViewModel {
+        return WeatherViewModel(cityName: response.name,
+                                temperature: "\(Int(response.main.temp))",
+                                feelsLike: "Ощущается как \(Int(response.main.feelsLike))",
+                                description: response.weather.first?.description.capitalized ?? "",
+                                tempMin: "Мин: \(Int(response.main.tempMin))",
+                                tempMax: "Макс: \(Int(response.main.tempMax))",
+                                humidity: "\(response.main.humidity)%",
+                                windSpeed: "\(Int(response.wind.speed)) м/с",
+                                weatherId: response.weather.first?.id ?? 800)
+    }
+}
---
+++ IOSWeatherApp/Features/Weather/Model/WeatherViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherViewModel.swift
+//  IOSWeatherApp
+//
+//  Created by Eldos Kolbay on 18.04.2026.
+//
+
+import Foundation
+
+
+struct WeatherViewModel {
+    let cityName: String
+    let temperature: String
+    let feelsLike: String
+    let description: String
+    let tempMin: String
+    let tempMax: String
+    let humidity: String
+    let windSpeed: String
+    let weatherId: Int
+}
